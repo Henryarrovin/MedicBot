37f2bfe62757eedf830edf667a18bfd5b83e512f
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-07-12 12:53:30 +0530

    Initial commit

---
+++ src/main/java/app/web/MedicBot/MedicBotApplication.java
@@ -0,0 +1,13 @@
+package app.web.MedicBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/web/MedicBot/MedicBotApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.MedicBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
