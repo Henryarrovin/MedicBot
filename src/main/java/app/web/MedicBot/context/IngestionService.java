8f4e1f9f2883ddf037f0e78ab93569111d3c9b7c
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-07-12 18:34:44 +0530

    Added Ingestion Service.

---
+++ src/main/java/app/web/MedicBot/context/IngestionService.java
@@ -0,0 +1,35 @@
+package app.web.MedicBot.context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngestionService implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(IngestionService.class);
+    private final VectorStore vectorStore;
+
+    @Value("classpath:docs/Oxford Handbook of Clinical Medicine.pdf")
+    private Resource clinicalMedicineResource;
+
+    public IngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var pdfReader = new ParagraphPdfDocumentReader(clinicalMedicineResource);
+        TextSplitter textSplitter = new TokenTextSplitter();
+        vectorStore.accept(textSplitter.apply(pdfReader.get()));
+        log.info("VectorStore Loaded with data!");
+    }
+
+}
